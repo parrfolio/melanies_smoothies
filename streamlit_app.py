1689175283fc5bfb325e2e01cd2ce8f2638c67a3
Ryan Parr <parrfolio@gmail.com>
2026-01-29 13:23:17 -0800

    Refactor fruit options data retrieval in streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,6 +96,13 @@ if st.session_state.order_submitted:
 if ingredients_list:
     st.header("🥗 Nutrition Information")
 
+    my_dataframe = (
+        session.table("smoothies.public.fruit_options")
+        .select(col("FRUIT_NAME"), col("SEARCH_ON"))
+    )
+
+    pd_df = my_dataframe.to_pandas()
+
     for fruit_chosen in ingredients_list:
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
@@ -106,13 +113,6 @@ if ingredients_list:
         )
 
         st.write(smoothiefruit_response.json())
-
-    my_dataframe = (
-        session.table("smoothies.public.fruit_options")
-        .select(col("FRUIT_NAME"), col("SEARCH_ON"))
-    )
-
-    pd_df = my_dataframe.to_pandas()
 
     st.dataframe(data=pd_df)
 
