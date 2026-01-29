8b7dead0308f3dcd5aff32433b12b22ad0aee823
Ryan Parr <parrfolio@gmail.com>
2026-01-29 13:22:03 -0800

    Add search value display for selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -97,6 +97,8 @@ if ingredients_list:
     st.header("🥗 Nutrition Information")
 
     for fruit_chosen in ingredients_list:
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(fruit_chosen + " Nutrition Information")
 
         smoothiefruit_response = requests.get(
