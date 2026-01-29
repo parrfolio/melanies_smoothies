f8ac877a77f406cc0333fcf7c1b40ade9870f839
Ryan Parr <parrfolio@gmail.com>
2026-01-29 12:48:41 -0800

    Fix typo in smoothiefruit_response variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,9 +96,9 @@ if st.session_state.order_submitted:
         st.rerun()
 
 
-#st.text(smoothiefroot_response.json())
+#st.text(smoothiefruit_response.json())
 
 sf_df = st.dataframe(
-    data=smoothiefroot_response.json(),
+    data=smoothiefruit_response.json(),
     use_container_width=True
 )
